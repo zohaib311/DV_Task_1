f3f1e9fde2bc1f612a253962e095889e39a3d68f
Zohaib Aslam <m.zohaibaslam02@gmail.com>
2026-09-14 22:50:59 +0500

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -58,7 +58,7 @@
         <li class="nav-item">
             <a href="{{ route('allResults') }}"
                 class="sidebar-link nav-link {{ request()->is('result*') ? 'active' : '' }}">
-                <i class="bi-bar-chart-steps me-2 fs-5"></i>
+                <i class="bi bi-window-stack me-2 fs-5"></i>
                 <span>Results</span>
             </a>
         </li>
